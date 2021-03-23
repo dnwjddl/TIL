7cf4d223ef4dd63ea0d5d370f4472493bb2b9924
dnwjddl <woojung980305@gmail.com>
2021-03-24 00:52:45 +0900

    Delete algorithm/LEVEL 1 directory

--- algorithm/LEVEL 1/3월 넷째주/가운데 글자 가져오기/code.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    b = len(s) // 2
-    if len(s) % 2 == 0:
-        answer = s[b-1:b+1]
-    else:
-        answer = s[b]
-    return answer
--- algorithm/LEVEL 1/3월 셋째주/2016년/code.py
+++
@@ -1,6 +0,0 @@
-import datetime
-import time
-
-def solution(a, b):
-    week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-    return week[datetime.date(2016,a,b).weekday()]
